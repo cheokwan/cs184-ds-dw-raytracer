a5a375f8c8ba4d0f641d9b63615ea53157ac8224
jasonwan415 <jasonwan415@0be72354-b10b-e666-8ac9-db255acbfd61>
2010-04-14 02:37:18 +0000

    Milestone done.

--- Camera.cpp
+++ Camera.cpp
@@ -1,1 +1,33 @@
 #include "Camera.h"
+#include <math.h>
+
+Camera::Camera(vec3 look_from, vec3 look_at, vec3 up,
+               float field_of_view, float aspect_ratio,
+               float z_near, float z_far) {
+  this->look_from = look_from;
+  this->look_at = look_at;
+  this->up = up;
+  this->field_of_view = field_of_view;
+  this->aspect_ratio = aspect_ratio;
+  this->z_near = z_near;
+  this->z_far = z_far;
+
+  look_dir = (look_at - look_from).normalize();
+  right = (look_dir ^ up).normalize();
+  up = (right ^ look_dir).normalize();
+  screen_x = aspect_ratio * tan(field_of_view) * z_near;
+  screen_y = tan(field_of_view) * z_near;
+}
+
+Camera::~Camera() {
+}
+
+void Camera::generateRay(vec2 sample, vec3& ray) {
+  ray = (z_near * look_dir) +
+        (screen_x * sample[0] * right) +
+        (screen_y * sample[1] * up);
+}
+
+vec3 Camera::position() {
+  return look_from;
+}
--- Scene.cpp
+++ Scene.cpp
@@ -2,23 +2,60 @@
 #include "Sampler.h"
 #include "Camera.h"
 
+#include "bmp/EasyBMP.h"
+#include "Sphere.h"
+
 #include <stdio.h>
 /*  Dedicated to running the milestone  */
 void milestone() {
-  int size_x = 160;
-  int size_y = 120;
-  vec3 cam_lookfrom(0.0, 0.0, -5.0);
-  vec3 cam_lookat(0.0, 0.0, 1.0);
+  int size_x = 800;
+  int size_y = 600;
+  vec3 cam_lookfrom(0.0, 0.0, 4.0);
+  vec3 cam_lookat(1.0, 0.0, -1.0);
   vec3 cam_up(0.0, 1.0, 0.0);
   float fov = 60.0;
 
-  Sampler test_sampler(3, 3, 2);
+
+  Sphere sphere = Sphere();
+  sphere.setRadius(2.0);
+  sphere.setCenter(vec3(0.0, 0.0, -16.0));
+
+  RGBApixel *red, *black;
+  red = (RGBApixel*) malloc (sizeof(RGBApixel));
+  red->Red = 255; red->Blue = 0; red->Green = 0; red->Alpha = 0;
+  black = (RGBApixel*) malloc (sizeof(RGBApixel));
+  black->Red = 0; black->Blue = 0; black->Green = 0; black->Alpha = 0;
+  BMP *myPic = new BMP();
+  myPic->SetSize(size_x, size_y);
+  int x = 0; int y = 0;
+
+
+  Sampler test_sampler(size_x, size_y, 1);
+  Camera camera(cam_lookfrom, cam_lookat, cam_up, fov, size_x / float(size_y), 1.0, 10.0);
   vec2 sample;
-  int i = 0;
+  vec3 ray;
   while (test_sampler.getSample(sample)) {
-    printf("sample x: %.2f,  y: %.2f\n", sample[0], sample[1]);
+    //printf("sample x: %.2f,  y: %.2f\n", sample[0], sample[1]);
+    camera.generateRay(sample, ray);
+    //printf("ray (%.2f, %.2f, %.2f)\n", ray[0], ray[1], ray[2]);
+
+    if (x == size_x) {
+      y++;
+      x = 0;
+    }
+    if (sphere.intersect(camera.position(), ray)) {
+      myPic->SetPixel(x, y, *red);
+    }
+    else {
+      myPic->SetPixel(x, y, *black);
+    }
+    x++;
   }
 
+  myPic->WriteToFile("milestone.bmp");
+  delete myPic;
+  free(red);
+  free(black);
 }
 
 int main(int argc, char** argv) {
