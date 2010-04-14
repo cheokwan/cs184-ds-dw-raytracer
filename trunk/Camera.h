6057ca8c2dc97a7a7316e509f1b23d2067ca1a05
jasonwan415 <jasonwan415@0be72354-b10b-e666-8ac9-db255acbfd61>
2010-04-14 02:37:18 +0000

    Milestone done.

--- trunk/Camera.h
+++ trunk/Camera.h
@@ -1,10 +1,30 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include "algebra3.h"
+
 class Camera {
+  vec3 look_from;
+  vec3 look_at;
+  vec3 up;
+  float field_of_view;
+  float aspect_ratio;
+  float z_near;
+  float z_far;
+
+  vec3 look_dir;
+  vec3 right;
+  float screen_x;
+  float screen_y;
+
  public:
   Camera();
+  Camera(vec3 look_from, vec3 look_at, vec3 up,
+         float field_of_view, float aspect_ratio,
+         float z_near, float z_far);
   ~Camera();
+  void generateRay(vec2 sample, vec3& ray);
+  vec3 position();
 };
 
 
