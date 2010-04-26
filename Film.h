9450f6dfac5ad75353520369ff51a1b0eed3bcf0
jasonwan415 <jasonwan415@0be72354-b10b-e666-8ac9-db255acbfd61>
2010-04-26 16:04:35 +0000

    Just some little progress last night.

--- Film.h
+++ Film.h
@@ -2,15 +2,21 @@
 #define FILM_H
 
 class Film {
-  char* colorbuffer;
   int width;
   int height;
   int depth;
+  char* color_buffer;
+  int total_samples;
+  int current_sample;
 
  public:
   Film();
   Film(int width, int height, int depth);
   ~Film();
+  /*  commit is performed in order only.
+      pixels are swept horizontally from upper left corner to lower right
+      corner, same order as the Sampler generates samples.   */
+  bool commit(char color);
 };
 
 #endif
